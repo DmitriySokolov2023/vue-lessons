8e6bc57855eb950c8e2b6bfaa3dc635bd1f4773e
DmitriySokolov2023 <sokolovoff2023@gmail.com>
2026-02-05 11:40:35 +0300

    123

--- my-proejct-2/app.js
+++ my-proejct-2/app.js
@@ -8,8 +8,16 @@ const App = {
 		},
 		items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 	}),
-	methods: {},
-	computed: {},
+	methods: {
+		addItem() {
+			this.items.unshift(this.$refs.myInput.value)
+		},
+	},
+	computed: {
+		evenItems() {
+			return this.items.filter(el => el % 2 === 0)
+		},
+	},
 	watch: {},
 }
 
